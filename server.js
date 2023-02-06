a856c7907f7a32589d0396f8d35a3d8b351c546a
Gabriela Fernandes Silva <gabi.fernandes@ibm.com>
2023-02-06 20:39:08 -0300

    config

--- server.js
+++ server.js
@@ -8,9 +8,10 @@ var ObjectID = require('mongodb').ObjectId;
 
 require('dotenv/config');
 
-app.use(express.json({ limit: '50mb' }));
-app.use(express.urlencoded({ limit: '50mb' }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(cors());
+app.use(express.json());
 const port = process.env.PORT || 8001;
 
 app.listen(process.env.PORT || 8001, () => {
