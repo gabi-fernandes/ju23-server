0511a74cd1dc2dea9370e25dea3e3de791c41732
VitorBosca <113400437+VitorBosca@users.noreply.github.com>
2023-03-06 20:57:25 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,169 +1,128 @@
-import logo from './logo.svg';
-import './App.css';
-import axios from 'axios';
+const express = require('express');
+const path = require('path');
+const cors = require('cors');
+const axios = require('axios');
+const app = express();
+var MongoClient = require('mongodb-legacy').MongoClient;
+var ObjectID = require('mongodb-legacy').ObjectId;
 
-import React, { useState } from 'react';
-import Capa from './capa.jpg';
-import Seta from './seta-esquerda.PNG';
+require('dotenv/config');
 
-import ReactLoading from 'react-loading';
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+app.use(express.json());
+const port = process.env.PORT || 8001;
 
-export default function App() {
-  const [comoChegar, setComoChegar] = useState(false);
-  const [confirme, setConfirme] = useState(false);
-  const [nome, setNome] = useState('');
-  const [qtPessoas, setQtPessoas] = useState(0);
-  const [ciente, setCiente] = useState('');
-  const [recado, setRecado] = useState('');
-  const [confirmado, setConfirmado] = useState(false);
-  const [disableButton, setDisableButton] = useState(false);
-  const [loading, setLoading] = useState(false);
+app.listen(process.env.PORT || 8001, () => {
+  console.log(`App UI available http://localhost:${port}`);
+});
 
-  async function vai() {
-    setDisableButton(true);
-    setLoading(true);
-    if (!isNaN(qtPessoas)) {
-      setQtPessoas(0);
-    }
-
-    let data = {
-      data: {
-        nome: nome,
-        qtPessoas: qtPessoas,
-        ciente: ciente,
-        recado: recado,
-      },
-    };
-    let response = await axios.post(`${process.env.REACT_APP_URL3}`, data);
-    if (response.status == 200) {
-      setLoading(false);
-      setConfirmado(true);
-    }
-    setNome('');
-    setQtPessoas('');
-    setCiente('');
-    setRecado('');
+const url =
+  'mongodb+srv://ju23:140300@ju23.wa4cvxv.mongodb.net/?retryWrites=true&w=majority';
+app.get('/getConvidados', (req, res, err) => {
+  console.log('GETCONVIDADOS');
+  try {
+    MongoClient.connect(url, function (err, db) {
+      if (err) throw err;
+      var dbo = db.db('convidados');
+      dbo
+        .collection('convidados')
+        .find({})
+        .toArray(function (err, result) {
+          if (err) throw 'err';
+          res.send(result);
+          db.close();
+        });
+    });
+  } catch (e) {
+    console.log(err);
   }
-  return (
-   /* <div className="App" id={confirmado ? 'blocked' : ''}>
-      {loading ? (
-        <div id="loading">
-          <ReactLoading type={'spin'} color="#fff" />
-        </div>
-      ) : (
-        ''
-      )}
-      {confirmado ? (
-        <>
-          <div id="confirmado">
-            <br></br>
-            Obrigada pela confirmação!<br></br> <br></br>Te espero lá ❤️
-          </div>
-          <div
-            onClick={() => {
-              setConfirmado(false);
-              setConfirme(false);
-              setDisableButton(false);
-            }}
-            id="close"
-          >
-            x
-          </div>
-        </>
-      ) : null}
+});
+app.post('/adicionar', (req, res, err) => {
+  try {
+    MongoClient.connect(url, function (err, db) {
+      if (err) throw err;
+      var dbo = db.db('convidados');
+      dbo
+        .collection('convidados')
+        .insertOne(req.body, function (err, response) {
+          if (err) throw err;
+          res.send(response);
+          db.close();
+        });
+    });
+  } catch (e) {
+    console.log(err);
+    res.send(e);
+  }
+});
 
-      <img src={Capa} id="capa" />
-      {!comoChegar && !confirme ? (
-        <div id="buttons">
-          <button onClick={() => setComoChegar(true)}>COMO CHEGAR</button>
-          <button onClick={() => setConfirme(true)}>
-            CONFIRME SUA PRESENÇA
-          </button>
-        </div>
-      ) : (
-        <>
-          {comoChegar ? (
-            <div id="como-chegar">
-              <img
-                onClick={() => setComoChegar(false)}
-                src={Seta}
-                height={30}
-                style={{ paddingLeft: '1rem', position: "absolute", top: "2rem", left: "1rem"}}
-              />
-              <div id="div-como-chegar">
-                <div id="texto-como-chegar">
-                  Use o Waze para dirigir até Espaço Canto Belo, SP!
-                </div>
-                <button>
-                  <a href="https://waze.com/ul/h6gy6wdym5" target="_blank">
-                    Clica aqui!
-                  </a>
-                </button>
-              </div>
-            </div>
-          ) : (
-            <div id="background">
-              <img
-                onClick={() => {
-                  setConfirme(false);
-                  setConfirmado(false);
-                  setDisableButton(false);
-                }}
-                src={Seta}
-                height={30}
-                style={{ paddingLeft: '1rem', position: "absolute", top: "2rem", left: "1rem"}}
-              />
-              <div id="confirme-presenca">
-                <p>Nome:</p>
-                <input onChange={(e) => setNome(e.target.value)} />
-                <p>
-                  Além de você mesmo(a)... Quantas pessoas você vai levar?{' '}
-                  <br></br> <br></br> (Em número por favor)
-                </p>
-                <input
-                  type={'number'}
-                  onChange={(e) => setQtPessoas(e.target.value)}
-                />
-                <p>
-                  Está ciente de que, caso esqueça o bikini, vai passar vontade
-                  de entrar na piscina, e caso esqueça a sua bebida, vai passar
-                  sede?
-                </p>
-                <div id="radio">
-                  <input
-                    type="radio"
-                    name="fav_language"
-                    value="SIM"
-                    onChange={(e) => setCiente(e.target.value)}
-                  />
-                  <label for="Sim">SIM</label>
-                  <input
-                    type="radio"
-                    name="fav_language"
-                    value="NÃO"
-                    onChange={(e) => setCiente(e.target.value)}
-                  />
-                  <label for="não">NÃO</label>
-                </div>
-                <p>Me deixa um recadinho, vai! ❤️</p>
-                <textarea
-                  id="recado"
-                  rows="5"
-                  onChange={(e) => setRecado(e.target.value)}
-                ></textarea>
-                <button
-                  id="enviar"
-                  onClick={vai}
-                  style={disableButton ? { backgroundColor: 'gray' } : null}
-                  disabled={disableButton ? true : false}
-                >
-                  Enviar!
-                </button>
-              </div>
-            </div>
-          )}
-        </>
-      )}
-    </div>*/
-  );
-}
+app.post('/confirmacao', (req, res, err) => {
+  console.log('/CONFIRMACAO');
+  try {
+    MongoClient.connect(url, function (err, db) {
+      if (err) throw err;
+      let newId = new ObjectID(req.body.data._id);
+      req.body.data._id = newId;
+      let usuario = req.body.data;
+      let confirmacao = req.body.confirmacao;
+      var myquery = usuario;
+      var newvalues = { $set: { vou: confirmacao } };
+      var dbo = db.db('convidados');
+      dbo
+        .collection('convidados')
+        .updateOne(myquery, newvalues, function (err, response) {
+          if (err) throw err;
+          res.send('1 document updated');
+          db.close();
+        });
+    });
+  } catch (e) {
+    console.log(err);
+  }
+});
+
+app.post('/entrou', (req, res, err) => {
+  console.log('/ENTROU');
+  try {
+    MongoClient.connect(url, function (err, db) {
+      if (err) throw err;
+      var query = { _id: ObjectID(req.body[0]) };
+      var newvalues = { $set: { ENTROU: 'S' } };
+      var dbo = db.db('convidados');
+      dbo
+        .collection('convidados')
+        .updateOne(query, newvalues, function (err, response) {
+          if (err) throw err;
+          res.send('1 document updated');
+          db.close();
+        });
+    });
+  } catch (e) {
+    console.log(err);
+  }
+});
+
+app.post('/check', (req, res, err) => {
+  console.log('/check');
+  try {
+    MongoClient.connect(url, function (err, db) {
+      if (err) throw err;
+      let id = ObjectID(req.body._id);
+
+      var query = { _id: ObjectID(req.body[0]) };
+      var dbo = db.db('convidados');
+      dbo
+        .collection('convidados')
+        .find(query)
+        .toArray(function (err, response) {
+          if (err) throw err;
+          res.send(response);
+          db.close();
+        });
+    });
+  } catch (e) {
+    console.log(err);
+  }
+});
